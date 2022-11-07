2e8743e59f9dfec26c9f78d50a79d205a1f26867
chatmongcol <cdadwngs@gmail.com>
2022-11-07 15:39:22 +0700

    update

---
+++ python401.py
@@ -0,0 +1,10 @@
+x = 10       #number
+y = 12       #Number  
+z = "10"     #String
+
+print(x > y)  #false
+print(x < y)  #true
+print(x == z) #false
+print(x != z) #true
+print(x >= y) #false
+print(x <= y) #true
